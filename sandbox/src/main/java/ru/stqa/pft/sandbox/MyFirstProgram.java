bc07b440901f716f0b30f1121e0d2d98962fea68
Elvira Mak <efilia@bk.ru>
2018-10-20 20:46:46 +0300

    Создан класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,13 +12,25 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(4,6);
     System.out.println("Площадь прямоугольника со сторонами " +r.a +" и " +r.b + " = " + r.area());
+
+
+
+    Point p1 = new Point (1,1);
+
+    Point p2 = new Point (2,2);
+
+    double d = p1.distance (p2);
+    System.out.println("Distance between p1(" +p1.x + "," + p1.y + ") and p2(" + p2.x + "," + p2.y + ") = " + d);
+
+
+
+
   }
 
   public static void hello ( String somebody) {
 
     System.out.println("Hello, "+ somebody + " ! ");
   }
-
 
 
 
