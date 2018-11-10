20df68a22cc23630d9efbe2c0640886d37d9d248
Elvira Mak <efilia@bk.ru>
2018-11-10 16:40:28 +0400

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -14,10 +14,10 @@ public class ContactCreationTests extends TestBase {
             new ContactGeneral("Elvira", "Makateva"),
             new ContactCompanyInfo("Bank", "Saratov"),
             new ContactNumber("+79008885522", "+79007775522", "+79005552255"),
-            new ContactEmails("mak@mail.ru", "mak1@mail.ru", "mak3@mail.ru"),
+            new ContactEmails("mak@mail.ru", "mak1@mail.ru", null),
             new ContactBirth("1", "December", "1990"),
             "Test1",
-            new ContactSecondary("Saratov2", "Saratov3", "else"), true);
+            new ContactSecondary(null, null, null), true);
     app.getContactHelper().submintContactCreation();
     app.getContactHelper().returnToContactPage();
     app.getSessionHelper().logout();
