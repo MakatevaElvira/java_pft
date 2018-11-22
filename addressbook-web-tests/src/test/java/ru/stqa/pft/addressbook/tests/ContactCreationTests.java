79a3225e04dc933fe17f99002cdfea94e6d762c4
Elvira Mak <efilia@bk.ru>
2018-11-22 21:28:56 +0400

    Преобразование вычисления макс ид в лямбда-выражение

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.model.*;
+import ru.stqa.pft.addressbook.model.ContactGeneral;
 
 import java.util.HashSet;
 import java.util.List;
@@ -14,19 +14,13 @@ public class ContactCreationTests extends TestBase {
 
     app.getNavigationHelper().goToContactPage();
     List<ContactGeneral> before = app.getContactHelper().getContactList();
-    ContactGeneral contactG = new ContactGeneral("Elvira", "Makateva");
+    ContactGeneral contactG = new ContactGeneral("Efilia", "Liopda");
     app.getContactHelper().createContactGeneral(contactG);
 
     List<ContactGeneral> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactGeneral c : after) {
-      if (c.getId()> max) {
-        max = c.getId();
-      }
-    }
-    contactG.setId(max);
+    contactG.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contactG);
     Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
 
