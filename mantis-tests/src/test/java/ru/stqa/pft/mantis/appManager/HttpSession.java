ec27b153cb21d8cc73a169a7a78c396f64694190
Elvira Mak <efilia@bk.ru>
2019-04-10 21:39:32 +0400

    Создан тест Логина через HTTP для Mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appManager/HttpSession.java
@@ -0,0 +1,94 @@
+package ru.stqa.pft.mantis.appManager;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HttpSession {
+  private CloseableHttpClient httpClient;
+  private ApplicationManager app;
+
+  public HttpSession(ApplicationManager app){
+    this.app = app;
+    httpClient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
+
+  }
+  public boolean loginL(String username, String password) throws IOException {
+    HttpPost post1 = new HttpPost(app.getProperty("web.baseUrl") + "login_page.php");
+    List <NameValuePair> params1 = new ArrayList<>();
+    params1.add(new BasicNameValuePair("username",username));
+   // params.add(new BasicNameValuePair("secure_session","on"));
+    //params.add(new BasicNameValuePair("password",password));
+   // params.add(new BasicNameValuePair("secure_session","on"));
+    params1.add(new BasicNameValuePair("return","index.php"));
+    post1.setEntity(new UrlEncodedFormEntity(params1));
+
+    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "login_password_page.php");
+    List <NameValuePair> params = new ArrayList<>();
+    //params.add(new BasicNameValuePair("username",username));
+    // params.add(new BasicNameValuePair("secure_session","on"));
+    params.add(new BasicNameValuePair("password",password));
+    // params.add(new BasicNameValuePair("secure_session","on"));
+    params.add(new BasicNameValuePair("return","index.php"));
+    post.setEntity(new UrlEncodedFormEntity(params));
+
+
+    CloseableHttpResponse response = httpClient.execute(post);
+    String body = geTextFrom(response);
+    return body.contains(String.format("<span class =\"user-info\">%s</span>",username)); // текст(%s)= это имя пользователя
+  }
+  public boolean loginP(String username, String password) throws IOException {
+    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "login_page.php");
+    List <NameValuePair> params = new ArrayList<>();
+    params.add(new BasicNameValuePair("username",username));
+    // params.add(new BasicNameValuePair("secure_session","on"));
+    params.add(new BasicNameValuePair("password",password));
+    params.add(new BasicNameValuePair("secure_session","on"));
+    params.add(new BasicNameValuePair("return","index.php"));
+    post.setEntity(new UrlEncodedFormEntity(params));
+    CloseableHttpResponse response = httpClient.execute(post);
+    String body = geTextFrom(response);
+    return body.contains(String.format("<span class =\"user-info\">%s</span>",username)); // текст(%s)= это имя пользователя
+  }
+
+  public boolean login(String username, String password) throws IOException {
+    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "login.php");
+    List <NameValuePair> params = new ArrayList<>();
+    params.add(new BasicNameValuePair("username",username));
+    params.add(new BasicNameValuePair("password",password));
+    params.add(new BasicNameValuePair("secure_session","on"));
+    params.add(new BasicNameValuePair("return","index.php"));
+    post.setEntity(new UrlEncodedFormEntity(params));
+    CloseableHttpResponse response = httpClient.execute(post);
+    String body = geTextFrom(response);
+    //System.out.println(body);
+    return body.contains(String.format("<span class=\"user-info\">%s</span>",username));
+   // return body.contains(String.format("//span[contains(text(),'administrator')]"));
+   // return body.contains(String.format("<span class = \"italic\">%s</span>",username)); // текст(%s)= это имя пользователя
+  }
+
+  private String geTextFrom(CloseableHttpResponse response) throws IOException {
+    try {
+     return EntityUtils.toString(response.getEntity());
+    } finally {
+      response.close();
+    }
+  }
+  public boolean isLoggedInAs(String username) throws IOException {
+    HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "login.php");
+    CloseableHttpResponse response = httpClient.execute(get);
+    String body = geTextFrom(response);
+    //return body.contains(String.format("<span class = \"italic\">%s</span>",username));
+    return body.contains(String.format("<span class=\"user-info\">%s</span>",username));
+  }
+}
