c9fa6f281c4153a9ae5600af860454ad4547b325
Elvira Mak <e.makateva@qiwi.com>
2020-02-24 16:30:15 +0300

    Задание 8. Проверка стикеров уточек

---
+++ selenium-tests/src/test/java/ru/stqa/pft/test/tests/MainPageTest.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.test.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class MainPageTest extends TestBase {
+    @Test
+    public void testStickerDucks() {
+        driver.navigate().to("http://localhost:8080/litecart/en/");
+
+        List<WebElement> ducks = driver.findElements(By.xpath("//div[@class='image-wrapper']"));
+        for (WebElement duck : ducks) {
+            List<WebElement>  stickers =  duck.findElements(By.cssSelector("div[class^='sticker']"));
+            Assert.assertEquals(stickers.size(),1);
+        }
+    }
+}
